dba494d89596f050c39b92597ecfae726edb6451
keysome-one <96916824+keysome-one@users.noreply.github.com>
2022-02-04 18:04:06 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,16 +47,6 @@ const StoreIndex = ({location}) => {
   
   
   
-  render: function () {
-    return React.createElement(
-      "div",
-      null,
-      React.createElement("link", { rel: "preconnect", href: "https://fonts.gstatic.com" }),
-      React.createElement("link", { href: "https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700;800&display=swap", rel: "stylesheet" }),
-      React.createElement("style", { dangerouslySetInnerHTML: { __html: "\n\t\n\tbody {\n\t  font-size: 3vh;\n\t  font-family: 'JetBrains Mono';\n\t  font-style: normal;\n\t  font-weight: 400;\n\t  font-display: swap;\n\t  src: url(https://fonts.gstatic.com/s/jetbrainsmono/v11/tDbY2o-flEEny0FZhsfKu5WU4zr3E_BX0PnT8RD8yKxTN1OVgaY.woff2) format('woff2');\n\t  unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;\n\t}\n\t" } })
-    );
-  }
-
 
   
   
@@ -81,6 +71,9 @@ const StoreIndex = ({location}) => {
             margin: '0 auto',
           }}
         >
+          
+  
+          
           <Image src={logo} alt="logo" />
         </Header.Content>
       </Header>
