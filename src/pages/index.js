100b52f408337cce935d70ab76be1c08dc34246d
keysome-one <96916824+keysome-one@users.noreply.github.com>
2022-02-04 17:47:50 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,4 +72,22 @@ const StoreIndex = ({location}) => {
   )
 }
 
+
+
+
+
+var NewComponent = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;700;800&display=swap" rel="stylesheet" />
+        <style dangerouslySetInnerHTML={{__html: "\n\t\n\tbody {\n\t  font-size: 3vh;\n\t  font-family: 'JetBrains Mono';\n\t  font-style: normal;\n\t  font-weight: 400;\n\t  font-display: swap;\n\t  src: url(https://fonts.gstatic.com/s/jetbrainsmono/v11/tDbY2o-flEEny0FZhsfKu5WU4zr3E_BX0PnT8RD8yKxTN1OVgaY.woff2) format('woff2');\n\t  unicode-range: U+0460-052F, U+1C80-1C88, U+20B4, U+2DE0-2DFF, U+A640-A69F, U+FE2E-FE2F;\n\t}\n\t" }} />
+      </div>
+    );
+  }
+});
+
+
+
 export default StoreIndex
